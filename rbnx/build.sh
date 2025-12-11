22b5e186ddfd21c67c23cbeaff1376a0c80d0b41
zhengwu <wuzheng_study@163.com>
2025-12-11 16:13:28 +0800

    move modified repo into submodule with patch fixed

--- rbnx/build.sh
+++ rbnx/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+git submodule update --init
+
+cd src/rtabmap_ros
+git apply ../my_rtabmap_ros.patch
+
+cd ../../
+
 source /opt/ros/humble/setup.bash
 rosdep update && rosdep install --from-paths src --ignore-src -r -y
 export MAKEFLAGS="-j8" # Can be ignored if you have a lot of RAM (>16GB)
